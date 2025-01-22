468f6e82fab2bf3192baf1afa928f0688d8d15a2
Cmd_ <uchzhezherov@gmail.com>
2025-01-22 20:29:56 +0300

    Disabled props

--- src/components/inlineButton/InlineButton.tsx
+++ src/components/inlineButton/InlineButton.tsx
@@ -1,11 +1,17 @@
 import { ReactElement } from "react";
 import s from "./InlineButton.module.css";
 
-function InlineButton(props: { children: ReactElement | string, isActive?: boolean, onClick?: (event: any) => void, className?: string}) {
+function InlineButton(props: {
+  children: ReactElement | string;
+  isActive?: boolean;
+  onClick?: (event: any) => void;
+  className?: string;
+  disabled?: boolean;
+}) {
   const { children } = props;
-  console.log(props.isActive);
   return (
     <button
+      disabled={props.disabled}
       className={`text-white text-start p-1 rounded ${s.button} ${
         props.isActive ? s.buttonActive : ""
       } ${props.className}`}
